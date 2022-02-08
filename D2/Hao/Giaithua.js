70d94274b4dcf1b4a662c656f7b0c31be8d865cd
tranvanhao016a <hao.tv1665@sinhvien.hoasen.edu.vn>
2022-02-08 17:06:13 +0700

    D2

---
+++ D2/Hao/Giaithua.js
@@ -0,0 +1,16 @@
+// var x= 5;
+// var giaithua = 1;
+// for(i = 1;i<=x;i++){
+//     giaithua = giaithua * i;
+// }
+// console.log(giaithua);
+
+function TinhGiaiThua( number){
+        var giaithua = 1;
+         for(var i = 1;i<=number;i++){
+           giaithua = giaithua * i;
+    }
+    console.log(giaithua);
+}
+
+ TinhGiaiThua(5);
