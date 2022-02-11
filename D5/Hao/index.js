96c033baebaf874192223d93eb1094a46977b5d3
tranvanhao016a <hao.tv1665@sinhvien.hoasen.edu.vn>
2022-02-11 21:24:16 +0700

    D5 - Nop bai web ban game

--- D5/Hao/index.js
+++ D5/Hao/index.js
@@ -1,27 +1,66 @@
 let group = [{
     name: "LMHT",
-    price: 150000,
-    photoURL: "./3.jpg"
+    price: "150.000",
+    photoURL: "./img/3.jpg"
 },
  {   name: "PUBG",
-    price: 175000,
-    photoURL: "./1.jpg",
+    price: "750.000",
+    photoURL: "./img/1.jpg",
 },
    { name: "GS",
-    price: 200000,
-    photoURL: "./4.jpg",
+    price: "200.000",
+    photoURL: "./img/4.jpg",
 },
 
 {
     name: "Ark",
-    price: 650000,
-    photoURL: "./5.jpg",
+    price: "650.000",
+    photoURL: "./img/5.jpg",
 },
 {
     name: "GTA 5",
-    price: 700000,
-    photoURL: "./6.jpg",
+    price: "700.000",
+    photoURL: "./img/6.jpg",
 },
+{
+    name: "Pokemon",
+    price: "600.000",
+    photoURL: "./img/2.jpg",
+},
+{
+    name: "Sifu",
+    price: "750.000",
+    photoURL: "./img/7.jpg",
+},
+{
+    name: "Left4Dead ",
+    price: "1.000.000",
+    photoURL: "./img/8.jpg",
+},
+{
+name: "NARAKA: BLADEPOINT",
+    price: "1.000.000",
+    photoURL: "./img/9.jpg",
+
+},
+{
+name: "Cyberpunk 2077",
+    price: "340.000",
+    photoURL: "./img/10.jpg",
+
+},
+{
+    name: "Call of Duty: Black Ops III",
+        price: "1.400.000",
+        photoURL: "./img/11.jpg",
+    
+    },
+    {
+        name: "NBA 2K21",
+            price: "1.340.000",
+            photoURL: "./img/12.jpg",
+        
+        },
 ];
 
 let container = document.getElementsByClassName("container");
@@ -34,7 +73,7 @@ for(let i = 0; i < group.length;i++){
 
     let name = document.createElement("h3");
     name.innerHTML = group[i].name;
-    name.style = "font-size:50px; color:#FF0000";
+    name.style = "font-size:30px; color:#FF0000";
 
     let price = document.createElement("h4");
 
@@ -48,10 +87,11 @@ for(let i = 0; i < group.length;i++){
 
     let buyBtn = document.createElement("button");
     buyBtn.innerHTML = "Mua";
-
-    itemBox1.appendChild(name);
-    itemBox1.appendChild(price);
     itemBox1.appendChild(img);
+    itemBox1.appendChild(price);
+    itemBox1.appendChild(name);
+   
+    
     itemBox1.appendChild(buyBtn);
 
     menuContainer[0].appendChild(itemBox1);
