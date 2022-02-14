928babb99d89d984383f486d8f89a0ecb9b69b7a
LeeKhoa1501 <khoa.lm0678@sinhvien.hoasen.edu.vn>
2022-02-14 10:56:35 +0700

    calculator leminhkhoa

---
+++ Demo Calculator_le minh khoa/script.js
@@ -0,0 +1,8 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+	button.addEventListener('click', (e) => {
+		switch()
+	}
+})
