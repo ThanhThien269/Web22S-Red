3882886ddf30b367912976691361932ddcc53d33
Duc Trong - Pham <trong.ptd3604@sinhvien.hoasen.edu.vn>
2022-02-14 11:01:34 +0700

    Delete Khoa

--- Demo Calculator_le minh khoa/script.js
+++
@@ -1,8 +0,0 @@
-let display = document.getElementById('display');
-let buttons = Array.from(document.getElementsByClassName('button'));
-
-buttons.map( button => {
-	button.addEventListener('click', (e) => {
-		switch()
-	}
-})
