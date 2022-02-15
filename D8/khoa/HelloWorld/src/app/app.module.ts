6ca1999062bc59ff9fd19752e0086b713cf9358f
LeeKhoa1501 <khoa.lm0678@sinhvien.hoasen.edu.vn>
2022-02-15 17:11:58 +0700

    Shop ban hang Le Minh Khoa

---
+++ D8/khoa/HelloWorld/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CounterComponent } from './components/counter/counter.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    CounterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
